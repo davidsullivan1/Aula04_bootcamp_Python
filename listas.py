36893066155567310017ec9612a3af5f298b6bcf
David Sullivan <david_sullivan@outlook.com>
2024-08-05 16:32:55 -0300

    Transformando listas em Json

--- listas.py
+++ listas.py
@@ -1,18 +1,48 @@
-produto1: str = "Sapato"
-produto2: str = "Camisa"
-produto3: str = "Calça"
-produto4: str = "Bota"
+# produto1: str = "Sapato"
+# produto2: str = "Camisa"
+# produto3: str = "Calça"
+# produto4: str = "Bota"
 
-produtos = []
-produtos.append(produto1)
-produtos.append(produto2)
-produtos.append(produto3)
-produtos.append(produto4)
+# produtos = []
+# produtos.append(produto1)
+# produtos.append(produto2)
+# produtos.append(produto3)
+# produtos.append(produto4)
 
-print(produtos)
+# print(produtos)
 
-#Exclui o último elemento adicionado na lista
-produtos.pop()
-#Exclui indicando o item que quer remover
-produtos.remove(produto1)
-print(produtos)
+# #Exclui o último elemento adicionado na lista
+# produtos.pop()
+# #Exclui indicando o item que quer remover
+# produtos.remove(produto1)
+# print(produtos)
+
+
+
+import json
+
+produto01: dict = {
+    "nome":"Sapato",
+    "Quantidade":10,
+    "Valor": 20.50
+}
+
+
+produto02: dict = {
+    "nome":"Camisa",
+    "Quantidade":5,
+    "Valor": 10.50
+}
+
+
+carrinho: list = []
+
+
+carrinho.append(produto01)
+carrinho.append(produto02)
+
+
+carrinho_json = json.dumps(carrinho)
+
+print(carrinho_json)
+
